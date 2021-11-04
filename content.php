eb4244b746c9469bd6187f766f94984e9558fa09
WidiantoBsi <widianto.mandosi@gmail.com>
2021-11-04 14:53:34 +0700

    first commit

---
+++ content.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <meta content="text/html; charset=UTF-8" http-equiv="content-type">
+</head>
+<body>
+    <div style="float: left;margin-right: 10px;">
+        <img src="cid:UBSI" alt="Logo" style="height: 50px">
+    </div>
+
+    <h2 style="margin-bottom: 0;">PortFolio Widianto</h2>
+    http://www.blogwidianto.biz.id
+
+    <div style="clear: both"></div>
+    <hr />
+
+    <div style="text-align: justify">
+        <?php echo  "Nama : ".$nama."<br>NoHp: ".$NoHp."<br>".$pesan; // Tampilkan isi pesan ?>
+    </div>
+</body>
+</html>
